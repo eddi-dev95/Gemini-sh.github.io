485f211fa0794eb8cfc326126e5b067931c1ce6b
Edisson Torres <87282774+eddi-dev95@users.noreply.github.com>
2024-07-31 18:48:59 -0500

    fix bugs

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-      // Fetch your API_KEY
-      const API_KEY = "AIzaSyBbvg1_n1CcmL-so3d3cFulwTYTBM_Ogg4"
+// Fetch your API_KEY
+const API_KEY = "AIzaSyBbvg1_n1CcmL-so3d3cFulwTYTBM_Ogg4";
 
-;
+// Access your API key (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(API_KEY);
 
-      // Access your API key (see "Set up your API key" above)
-      const genAI = new GoogleGenerativeAI(API_KEY);
-
-      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 async function run() {
-  const prompt = "Write a story about an AI and magic"
+  const prompt = "Write a story about an AI and magic";
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const text = response.text();
-  document.getElementById("mensaje").text = mensaje;
+  document.getElementById("mensaje").textContent = text;
   //console.log(text);
 }
 
 run();
+
