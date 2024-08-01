dc24f3c5fdfa0314a37ced716e9802f193bf9c79
Edisson Torres <87282774+eddi-dev95@users.noreply.github.com>
2024-08-01 14:06:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const GEMINI_API_KEY = '';
+const GEMINI_API_KEY = `${{ secrets.GEMINI_API_KEY }}`;
 const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
 const data = {
       "contents": [{
