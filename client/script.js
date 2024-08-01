7aa400c3c3e78cc94bc30a5145f5a8695fd6f8ad
Edisson Torres <87282774+eddi-dev95@users.noreply.github.com>
2024-08-01 09:09:43 -0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,34 +1,18 @@
-const apiKey = '';
-// Función para hacer la solicitud POST
-async function GetRequest(texto) {
-  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
-
-  const headers = {
-    'Content-Type': 'application/json',
-  };
-
-  const body = {
-      'contents': [{
-        'parts':[{
-          'text': 'Escribe un mensaje de despedida a una introduccion de un atajo de apple. Ejemplo: ¡Espero que lo disfutes!. No uses markdown, quiero el texto limpio, legible, amigable y llamativo'}]}]
-  };
-
+const getGeminiMessage = async () => {
   try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: headers,
-      body: JSON.stringify(body)
-    });
-
+    const response = await fetch('/api/gemini'); 
     const data = await response.json();
-    const  
- mensaje = data.candidates[0].content.parts[0].text;
+
+    // Accede al mensaje desde la respuesta JSON
+    const mensaje = data.mensaje;  
+
+    // Inserta el mensaje en el HTML
     const elementoMensaje = document.getElementById('mensaje');
     elementoMensaje.textContent = mensaje;
-  } catch (error) {
-    console.error('Error al hacer la solicitud:', error);
-  }
-}
 
-// Llama a la función para mostrar el mensaje
-GetRequest();
+  } catch (error) {
+    console.error('Error al obtener mensaje de Gemini:', error);
+  }
+};
+
+getGeminiMessage();
